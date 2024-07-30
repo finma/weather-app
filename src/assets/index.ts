9e57d7cdf836639ab527bd0343db8ac5f819e383
finma <firmanmaulana2403@gmail.com>
2024-07-30 13:01:08 +0700

    [ADD] index to assets

---
+++ src/assets/index.ts
@@ -0,0 +1,19 @@
+import clearIcon from "../../assets/clear.png";
+import cloudIcon from "../../assets/cloud.png";
+import drizzleIcon from "../../assets/drizzle.png";
+import humidityIcon from "../../assets/humidity.png";
+import rainIcon from "../../assets/rain.png";
+import searchIcon from "../../assets/search.png";
+import snowIcon from "../../assets/snow.png";
+import windIcon from "../../assets/wind.png";
+
+export {
+  clearIcon,
+  cloudIcon,
+  drizzleIcon,
+  humidityIcon,
+  rainIcon,
+  searchIcon,
+  snowIcon,
+  windIcon,
+};
